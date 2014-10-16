6fd9b87e322b7222e0e0e18ad6570bc59fb02ba3
ViniciusBaptista <vbaptista7@gmail.com>
2014-10-16 18:50:33 -0300

    Estrutura Sequencial: Décimo Sétimo Exercício

---
+++ python-brasil/EstruturaSequencial/decimo_setimo.rb
@@ -0,0 +1,15 @@
+def loja(metros)
+  cobertura = metros / 6
+  qts_latas = cobertura / 18
+  qts_galoes = (cobertura / 3.6).round
+
+  if cobertura < 18
+    return "Latas necessarias: 1 \n Valor total em latas: R$80 
+    \n Galões necessarios: #{qts_galoes} \n Valor total em galões: R$ #{qts_galoes * 25}"
+  end
+
+  "Latas necessarias: #{qts_latas} \n Valor total em latas: R$#{qts_latas * 80}
+  \n Galões necessarios: #{qts_galoes} \n Valor total em galões: R$ #{qts_galoes * 25}"
+end
+
+puts loja(80)
